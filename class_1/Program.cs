1f85c5729f2c78d935d4317d95c95b3ebca0d50f
leimeterjoaquin <leimeter.joaquin@gmail.com>
2022-04-05 15:41:49 -0300

    private attributes, getter setter methods

--- class_1/Program.cs
+++ class_1/Program.cs
@@ -6,15 +6,11 @@ namespace class_1
     {
         class Knight
         {
-            // public fields/attributes
-            public int row;
-            public int column;
-            public string color;
-
             // private fields/attributes
-            //private int row;
-            //private int column;
-            //private string color;
+            private bool captured;
+            private int row;
+            private int column;
+            private string color;
 
             //constructor
             public Knight(int initialRow, int initialColumn, string initialColor)
@@ -22,19 +18,49 @@ namespace class_1
                 row = initialRow;
                 column = initialColumn;
                 color = initialColor;
+                captured = false;
             }
 
-            //getters
-
-            //setters
-
-            //methods
+            //getter methods
+            public int getRow()
+            {
+                return row;
+            }
+            public int getColumn()
+            {
+                return column;
+            }
+            public string getColor()
+            {
+                return color;
+            }
+            public bool getCaptured()
+            {
+                return captured;
+            }
+            //setter methods
+            public void setPosition(int newRow, int newColumn)
+            {
+                //validations for row and column
+                //...
+                //...
+                row = newRow;
+                column = newColumn;
+            }
+            public void capture()
+            {
+                captured = true;
+            }
         }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             Knight knight1 = new Knight(4, 0, "black");
-            Console.WriteLine(knight1.row);
+            Console.WriteLine(knight1.getRow());
+            Console.WriteLine("is it captured?" + knight1.getCaptured());
+            Console.WriteLine("I'm capturing this knight...");
+            knight1.capture();
+            Console.WriteLine("is it captured?" + knight1.getCaptured());
         }
     }
 }
